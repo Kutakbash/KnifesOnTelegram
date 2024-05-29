f2291be4434f60b7ca8274296380f965f360933d
Kutakbash <almazs.kingdom.come@gmail.com>
2024-05-29 20:02:09 +0300

    /trying new

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "7065595742:AAFSey-iWFOytqBvuMhNtvS6os0F_yAqrUw";
 const server = express();
+const TOKEN = "527535160:AAGSUVLv_XqISVuKWFcdebEdz-_4YGeuHME";
 const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
-const gameName = "@KnifesOnTelegram_bot";
-const queries = {};
+const gameName = "KnifesOnTelegram";
+let queries = {};
 server.use(express.static(path.join(__dirname, 'KnifesOnTelegram')));
-bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
+bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Type /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
-bot.on("callback_query", function (query) {
+bot.on("callback_query",  query => {
     if (query.game_short_name !== gameName) {
-        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+        return bot.answerCallbackQuery(query.id, `Sorry, ${query.game_short_name} is not available.`);
     } else {
         queries[query.id] = query;
         let gameurl = "https://kutakbash.github.io/KnifesOnTelegram/";
-        bot.answerCallbackQuery({
+        return bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
         });
     }
 });
-bot.on("inline_query", function (iq) {
-    bot.answerInlineQuery(iq.id, [{
+bot.on("inline_query",  iq => {
+    return bot.answerInlineQuery(iq.id, [{
         type: "game",
         id: "0",
         game_short_name: gameName
     }]);
 });
-server.get("/highscore/:score", function (req, res, next) {
+server.get("/highscore/:score",  (req, res, next) => {
     if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
     let query = queries[req.query.id];
     let options;
@@ -45,7 +45,7 @@ server.get("/highscore/:score", function (req, res, next) {
             inline_message_id: query.inline_message_id
         };
     }
-    bot.setGameScore(query.from.id, parseInt(req.params.score), options,
+    return bot.setGameScore(query.from.id, parseInt(req.params.score), options,
         function (err, result) {});
 });
 server.listen(port);
